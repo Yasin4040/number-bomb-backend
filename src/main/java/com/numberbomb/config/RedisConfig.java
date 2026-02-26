c0c7e84806a1317a18062bc96790ddc80dbeaef2
李子耀 <673170360@qq.com>
2026-02-26 13:00:59 +0800

    完善相关配置文件4

--- src/main/java/com/numberbomb/config/RedisConfig.java
+++ src/main/java/com/numberbomb/config/RedisConfig.java
@@ -1,14 +1,49 @@
 package com.numberbomb.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+    
+    @Value("${spring.redis.host:120.27.196.197}")
+    private String redisHost;
+    
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+    
+    @Value("${spring.redis.password:li3301359}")
+    private String redisPassword;
+    
+    @Value("${spring.redis.database:0}")
+    private int redisDatabase;
+    
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            config.setPassword(redisPassword);
+        }
+        config.setDatabase(redisDatabase);
+        
+        logger.info("Redis配置 - Host: {}, Port: {}, Database: {}", redisHost, redisPort, redisDatabase);
+        
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
+        return factory;
+    }
     
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
